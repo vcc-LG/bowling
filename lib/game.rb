c8ab50d4253a6d831fc10604f20682a1052b1d2d
Leo Garcia <leo.garcia@immersivelabs.com>
2022-05-03 11:04:18 +0000

    Add Frame class

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,29 @@
 class Game
   def initialize
     @score = 0
+    @frames = []
   end
 
   def roll(pins)
-    @score += pins
+    current_frame = Frame.new(score: pins)
+    @frames << current_frame
   end
+
+  def score
+    @frames.map(&:score).sum
+  end
+end
+
+class Frame
+  def initialize(score:)
+    @score = score
+  end
+
+  def is_strike?; end
+
+  def is_spare?; end
+
+  def is_complete?; end
 
   attr_reader :score
 end
