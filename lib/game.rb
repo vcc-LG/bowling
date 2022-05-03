2a913b561d1d7ad1af410830c93485f22c173c6a
Leo Garcia <leo.garcia@immersivelabs.com>
2022-05-03 11:12:18 +0000

    Add additional logic to classes

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,18 @@
 
 class Game
   def initialize
-    @score = 0
     @frames = []
   end
 
   def roll(pins)
-    current_frame = Frame.new(score: pins)
-    @frames << current_frame
+    if @frames.empty? || @frames.last.is_complete?
+      current_frame = Frame.new
+      @frames << current_frame
+    else
+      current_frame = @frames.last
+    end
+
+    current_frame.roll(pins: pins)
   end
 
   def score
@@ -17,15 +22,23 @@ class Game
 end
 
 class Frame
-  def initialize(score:)
-    @score = score
+  def initialize
+    @score = 0
+    @roll_number = 0
+  end
+
+  def roll(pins:)
+    @score += pins
+    @roll_number += 1
   end
 
   def is_strike?; end
 
   def is_spare?; end
 
-  def is_complete?; end
+  def is_complete?
+    @roll_number == 2
+  end
 
   attr_reader :score
 end
